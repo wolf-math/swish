c95be81032d42e0af4e2802f85d1b65b76b1cbf5
Siel Goldofsky <sielgold@gmail.com>
2019-12-02 12:12:33 +0200

    fir commit in api

---
+++ app/services/youtube_api.rb
@@ -0,0 +1,13 @@
+class YoutubeApi
+
+  BASE = "https://www.googleapis.com/youtube/v3/search/?key=#{YOUTUBE_API}&part=snippet"
+
+end
+
+
+
+
+# https://www.googleapis.com/youtube/v3/search/?key=AIzaSyBAynJxXy0JHJpZtXVJpktLAcXalMpWmcY&part=snippet&channelId=UCT83YP07yVuaH9J19YABhlw
+
+
+https://www.googleapis.com/youtube/v3/videos/?key=AIzaSyBAynJxXy0JHJpZtXVJpktLAcXalMpWmcY&part=snippet&id=JE23ajVX930
