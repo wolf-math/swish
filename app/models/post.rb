da144a66943a4cac772bfd4a53f61e69c18b9664
Ron Artenstein <ronartenstein@gmail.com>
2019-12-09 12:43:58 +0200

    add photo to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :professional, optional: true
-
   has_many :comments, as: :commentable
   validates :title, presence: true
+  mount_uploader :photo, PhotoUploader
 end
---
+++ db/migrate/20191209103232_change_column_to_photo.rb
@@ -0,0 +1,5 @@
+class ChangeColumnToPhoto < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :posts, :image_url, :photo
+  end
+end
