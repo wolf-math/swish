4985dcb5233db2dac6682d8d120112a605185df2
Aaron Wolf <aaron.wolf@gmail.com>
2019-12-10 16:53:49 +0200

    dashboard. still needs work.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,9 @@
 class DashboardsController < ApplicationController
   def dashboard
-    @myposts = current_user.posts.order(created_at: :desc)
     skip_authorization
+    @myposts = current_user.posts.order(created_at: :desc)
+    @comments = current_user.comments.order(created_at: :desc)
+    @teams = current_user.follows.where(followable_type: "Team")
+    @players = current_user.follows.where(followable_type: "Person")
   end
 end
