c86ab0d174765bf75d8bb724073df4bc93a918a6
Ron Artenstein <ronartenstein@gmail.com>
2019-12-10 19:28:06 +0200

    sticky scroll and favicon

--- app/assets/javascripts/player_images.js
+++
@@ -1,9 +0,0 @@
-
-// unction getStats(){
-// nba.getPlayerHeadshotURL({PlayerID: 201935, TeamID: 1610612745})
-// }
-// alert("test");
-
-// nba.teamDetails({TeamID: 1610612745}).then(function(data){
-//   console.log(data)
-// })
