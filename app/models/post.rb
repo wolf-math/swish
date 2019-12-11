4959a84fe0f687503a4e3e5044fd3eebb369bb09
Aaron Wolf <aaron.wolf@gmail.com>
2019-12-11 15:46:45 +0200

    fixed form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user, optional: true
-  belongs_to :team, optional:true
-  belongs_to :person, optional:true
+  belongs_to :team, optional: true
+  belongs_to :person, optional: true
   # belongs_to :people, source: "Person"
   has_many :comments, as: :commentable
   validates :title, presence: true
-  mount_uploader :photo, PhotoUploader
+  mount_uploader :photo, PhotoUploader, optional: true
 
   def person_id
     people_id
